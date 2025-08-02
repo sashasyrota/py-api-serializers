48c178524d8559dd64bc1b43d37718c30912e496
Aleksandr <sasha.syrota15@gmail.com>
2025-08-02 15:01:03 +0300

    add SerializerMethodField

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -16,9 +16,14 @@ class GenreSerializer(serializers.ModelSerializer):
 
 
 class ActorSerializer(serializers.ModelSerializer):
+    full_name = serializers.SerializerMethodField()
+
     class Meta:
         model = Actor
         fields = ("id", "first_name", "last_name", "full_name")
+
+    def get_full_name(self, obj):
+        return obj.full_name
 
 
 class MovieSerializer(serializers.ModelSerializer):
